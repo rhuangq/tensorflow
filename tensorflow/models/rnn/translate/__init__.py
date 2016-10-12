8d4f9bbf40e11c5fe8f7705ad5dc5149f4a01f3e
Ron Huang <huangr@apple.com>
2016-10-12 12:02:26 -0400

    restore the models/rnn/translate to the upstream

--- tensorflow/models/rnn/translate/__init__.py
+++ tensorflow/models/rnn/translate/__init__.py
@@ -19,6 +19,4 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow.models.rnn.translate import data_utils
-from tensorflow.models.rnn.translate import data_utils_qnn
 from tensorflow.models.rnn.translate import seq2seq_model
-from tensorflow.models.rnn.translate import seq2seq_attention
